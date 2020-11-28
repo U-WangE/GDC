ac2aeaae6a13c148034d0f5a78da0faf633ef947
U-WangE <chamchamm@naver.com>
2020-11-29 02:35:52 +0900

    first

---
+++ app/src/main/java/com/example/grow_datacenter/GameActivity.java
@@ -0,0 +1,16 @@
+package com.example.grow_datacenter;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+    }
+}
